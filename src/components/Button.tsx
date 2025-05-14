0dc29c7380bc416244db9f78991d1e5e64ca5075
jakeorch <jakeorchanianatjake@gmail.com>
2025-05-13 18:35:43 -0700

    fix deployment error

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,4 +1,4 @@
-import React, { forwardRef } from "react";
+import React from "react";
 
 type ButtonProps = {
   children: React.ReactNode;
@@ -6,16 +6,15 @@ type ButtonProps = {
   onClick: () => void;
 };
 
-const Button = forwardRef<HTMLButtonElement, ButtonProps>((props, ref) => {
+function Button(props: ButtonProps) {
   return (
     <button
       onClick={props.onClick}
       className={`${props.className} flex bg-gray-200 hover:bg-gray-300 active:bg-gray-400/50 text-black text-center justify-center rounded-lg px-3.5 py-1 transition-all`}
-      ref={ref}
     >
       {props.children}
     </button>
   );
-});
+}
 
 export default Button;
