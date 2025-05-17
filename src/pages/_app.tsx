d42b36107cfce25c1ecd08fc28116416fbe0d763
jakeorch <jakeorchanianatjake@gmail.com>
2025-05-17 07:39:38 -0700

    add vercel analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,6 +5,8 @@ import { config } from "@fortawesome/fontawesome-svg-core";
 import "@fortawesome/fontawesome-svg-core/styles.css";
 config.autoAddCss = false;
 
+import { Analytics } from "@vercel/analytics/react";
+
 import { Lexend } from "next/font/google";
 
 const lexend = Lexend({ subsets: ["latin"] });
@@ -13,6 +15,7 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <main className={lexend.className}>
       <Component {...pageProps} />
+      <Analytics />
     </main>
   );
 }
